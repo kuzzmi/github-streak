7ce5b896ae1ecd5e74c2f48589b0bf32f4910db6
Igor Kuzmenko <igor@kuzzmi.com>
2016-12-02 21:22:15 +0100

    Passing tests and bare streaker.js

--- streaker.js
+++ streaker.js
@@ -1,9 +1,29 @@
-const contributions = {};
+const byAuthor = {};
+const usedHashes = {};
+
+module.exports.empty = () => {
+    const authors = Object.keys(byAuthor);
+    const hashes = Object.keys(usedHashes);
+    authors.forEach(author => {
+        delete byAuthor[author];
+    });
+    hashes.forEach(hash => {
+        delete usedHashes[hash];
+    });
+};
 
 module.exports.addContribution = (contribution) => {
-    const { author } = contribution;
-    contributions[author] = contribution;
+    const { author, sha1 } = contribution;
+
+    if (!usedHashes[sha1]) {
+        byAuthor[author] = byAuthor[author] || [];
+        byAuthor[author].unshift(contribution);
+        usedHashes[sha1] = true;
+    }
+
+    return byAuthor;
 };
+
 module.exports.getContributions = () => {
-    return contributions;
+    return byAuthor;
 };
--- tests/streaker.spec.js
+++ tests/streaker.spec.js
@@ -2,12 +2,32 @@ const expect = require('chai').expect;
 const streaker = require('../streaker');
 
 describe('streaker', () => {
-    describe('#getContributions()', () => {
-        it('should be defined', () => {
-            expect(streaker.getContributions).to.not.be.an('undefined');
+    describe('#empty()', () => {
+        beforeEach(() => {
+            streaker.empty();
         });
-        it('should return an object using author names as keys', () => {
-            const author = 'test';
+
+        it('should be defined', () => {
+            expect(streaker.empty).to.not.be.an.undefined;
+        });
+
+        it('should empty the map of contributions', () => {
+            streaker.addContribution({
+                author: 'test',
+                title: 'test',
+                sha1: 'test',
+                branch: 'test'
+            });
+
+            streaker.empty();
+
+            expect(Object.keys( streaker.getContributions() ).length).to.equal(0);
+        });
+    });
+
+    describe('#getContributions()', () => {
+        beforeEach(() => {
+            streaker.empty();
 
             streaker.addContribution({
                 author: 'test',
@@ -19,38 +39,98 @@ describe('streaker', () => {
             streaker.addContribution({
                 author: 'test2',
                 title: 'test',
-                sha1: 'test',
+                sha1: 'test2',
                 branch: 'test'
             });
+        });
 
+        it('should be defined', () => {
+            expect(streaker.getContributions).to.not.be.an.undefined;
+        });
+
+        it('should return an object using author names as keys', () => {
             expect(streaker.getContributions()).to.have.all.keys([ 'test', 'test2' ]);
         });
     });
+
     describe('#addContribution()', () => {
+        beforeEach(() => {
+            streaker.empty();
+        });
+
         it('should be defined', () => {
             expect(streaker.addContribution).to.not.be.an('undefined');
         });
+
         it('should add a contribution unit to the contribution collection', () => {
+            streaker.addContribution({
+                author: 'test',
+                title: 'test',
+                sha1: 'test',
+                branch: 'test'
+            });
+
+            expect(Object.keys( streaker.getContributions() ).length).to.equal(1);
+        });
+
+        it('should add a contribution unit to the same author if present', () => {
             const author = 'test';
 
             streaker.addContribution({
-                author: 'test',
+                author,
                 title: 'test',
                 sha1: 'test',
                 branch: 'test'
             });
 
-            expect(streaker.getContributionsFor().length).to.equal(1);
-        });
-        it('should add a contribution unit to the contribution collection', () => {
             streaker.addContribution({
-                author: 'test',
+                author,
+                title: 'test',
+                sha1: 'test1',
+                branch: 'test'
+            });
+
+            expect(streaker.getContributions()[author].length).to.equal(2);
+        });
+
+        it('should not add a contribution with existing SHA1 commit', () => {
+            const author = 'test';
+
+            streaker.addContribution({
+                author,
                 title: 'test',
                 sha1: 'test',
                 branch: 'test'
             });
 
-            expect(streaker.getContributions().length).to.equal(1);
+            streaker.addContribution({
+                author,
+                title: 'test',
+                sha1: 'test',
+                branch: 'test'
+            });
+
+            expect(streaker.getContributions()[author].length).to.equal(1);
+        });
+
+        it('should add contributions in a way that the last contribution from author is his oldest commit', () => {
+            const author = 'test';
+
+            streaker.addContribution({
+                author,
+                title: 'test',
+                sha1: 'test',
+                branch: 'test'
+            });
+
+            streaker.addContribution({
+                author,
+                title: 'test2',
+                sha1: 'test2',
+                branch: 'test'
+            });
+
+            expect(streaker.getContributions()[author][0].title).to.equal('test2');
         });
     });
 });
